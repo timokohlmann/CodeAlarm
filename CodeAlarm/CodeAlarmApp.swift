47e14edce23259214571513ce831d5e99b8a094e
Timo Kohlmann <kohlmann.timo@googlemail.com>
2025-07-21 12:32:37 +0200

    Initial commit

---
+++ CodeAlarm/CodeAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeAlarmApp.swift
+//  CodeAlarm
+//
+//  Created by Timo Kohlmann on 21.07.25.
+//
+
+import SwiftUI
+
+@main
+struct CodeAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeAlarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeAlarm
+//
+//  Created by Timo Kohlmann on 21.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
